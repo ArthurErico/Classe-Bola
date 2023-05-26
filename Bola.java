97995954a546396a42ff6810cc435973ead09c85
Arthur Erico <126835261+ArthurErico@users.noreply.github.com>
2023-05-26 20:09:37 -0300

    Create Bola.java

---
+++ Bola.java
@@ -0,0 +1,13 @@
+public class Bola {
+  String cor;
+  dubler circunferencia;
+  String material;
+  
+  void trocaCor(String novaCor) {
+    cor = novaCor;
+  }
+
+  String mostraCor() {
+    return cor;
+  }
+}
